c4bccde08215e0f83746d363de4e2e60abc64528
kalburgimanjunath <manjunathkalburgi65512@gmail.com>
2021-11-06 17:35:40 +0530

    1 file modified

--- src/common/Posts.js
+++ src/common/Posts.js
@@ -24,6 +24,28 @@ export default function Posts() {
       ],
       likes: 1000,
     },
+    {
+      id: 1,
+      title: 'Hello world post 1',
+      url: '',
+      created_at: '10th July',
+      tags: ['money', 'only', 'honey'],
+      comments: [
+        {
+          id: 1,
+          comments: 'hello manju 123',
+        },
+        {
+          id: 2,
+          comments: 'hello manju 123',
+        },
+        {
+          id: 3,
+          comments: 'hello manju 123',
+        },
+      ],
+      likes: 1000,
+    },
   ];
   return (
     <div>
